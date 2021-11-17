49d88b9506783624f1eb4d46ea2fdd9e82996c85
Lotx-001 <72420846+Lotx-001@users.noreply.github.com>
2021-11-17 18:06:55 +0300

    Update hyundaican.py

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -38,6 +38,12 @@ def create_lkas11(packer, frame, car_fingerprint, apply_steer, steer_req,
     # Note: the warning is hidden while the blinkers are on
     values["CF_Lkas_SysWarning"] = 4 if sys_warning else 0
 
+  elif car_fingerprint == CAR.K7:
+    values["CF_Lkas_LdwsActivemode"] = 2
+    #values["CF_Lkas_SysWarning"] = 2 if sys_warning else 0
+    values["CF_Lkas_FcwOpt_USM"] = 2
+    values["CF_Lkas_LdwsOpt_USM"] = 2
+
   elif car_fingerprint == CAR.GENESIS:
     # This field is actually LdwsActivemode
     # Genesis and Optima fault when forwarding while engaged
